24014d941ee39f989fb9e27d756f1be44a2680d3
Matthew Sessions <shichongrui@gmail.com>
2015-11-17 19:14:46 -0700

    Adds support for integer validation rules

--- strategies/integer.js
+++ strategies/integer.js
@@ -1,3 +1,36 @@
-module.exports = function () {
-  return Math.round(Math.random() * 100)
+module.exports = function (schema) {
+  var max = getMax(schema)
+  var min = getMin(schema)
+  var num = Math.floor(Math.random() * (max - min) + min)
+
+  if (schema.multipleOf) {
+    var multiple = Math.round(schema.multipleOf)
+    num = num * multiple
+  }
+
+  return num
+}
+
+function getMax(schema) {
+  var exclusiveMax = Boolean(schema.exclusiveMaximum)
+  var max = Math.floor(schema.maximum) || 100
+  if (schema.multipleOf) {
+    var multiple = schema.multipleOf
+    max = Math.floor(max / multiple)
+  }
+
+  return max
+}
+
+function getMin(schema) {
+  var exclusiveMin = Boolean(schema.exclusiveMinimum)
+  var min = Math.ceil(schema.minimum) || 0
+  if (schema.multipleOf) {
+    var multiple = schema.multipleOf
+    min = Math.ceil(min / multiple)
+  }
+
+  // if we are excluding min, increase min by a small amount to make it
+  // impossible to get it
+  return min + (exclusiveMin ? 1 : 0)
 }
--- tests/integer.spec.js
+++ tests/integer.spec.js
@@ -16,3 +16,76 @@ tape('integer creates whole numbers', function (t) {
   t.equal(integer(schema) % 1, 0)
   t.end()
 })
+
+tape('integer respects the minimum value', function (t) {
+  var min = 95
+  var schema = {
+    type: 'integer',
+    minimum: min
+  }
+  var allAbove = true
+  for (var i = 0; i < 100 && allAbove; i++) {
+    allAbove = integer(schema) >= min
+  }
+
+  t.equal(allAbove, true)
+  t.end()
+})
+
+tape('integer respects the exclusiveMinimum value', function (t) {
+  var min = 95
+  var schema = {
+    type: 'integer',
+    minimum: min,
+    exclusiveMinimum: true
+  }
+  var allAbove = true
+  for (var i = 0; i < 100 && allAbove; i++) {
+    allAbove = integer(schema) > min
+  }
+
+  t.equal(allAbove, true)
+  t.end()
+})
+
+tape('integer respects the maximum value', function (t) {
+  var max = 5
+  var schema = {
+    type: 'integer',
+    maximum: max
+  }
+  var allBelow = true
+  for (var i = 0; i < 100 && allBelow; i++) {
+    allBelow = integer(schema) <= max
+  }
+
+  t.equal(allBelow, true)
+  t.end()
+})
+
+tape('integer respects the exclusiveMaximum value', function (t) {
+  var max = 5
+  var schema = {
+    type: 'integer',
+    maximum: max,
+    exclusiveMaximum: true
+  }
+  var allBelow = true
+  for (var i = 0; i < 100 && allBelow; i++) {
+    allBelow = integer(schema) < max
+  }
+
+  t.equal(allBelow, true)
+  t.end()
+})
+
+tape('integer respects multipleOf value', function (t) {
+  var multiple = 2
+  var schema = {
+    type: 'integer',
+    multipleOf: multiple
+  }
+
+  t.equal(integer(schema) % multiple, 0)
+  t.end()
+})
