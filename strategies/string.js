df8e4374619a3e5ce0d15830210e5d1c7d600901
Matthew Sessions <msessions@instructure.com>
2016-01-13 11:41:54 -0700

    Add all validation rules for strings

--- strategies/string.js
+++ strategies/string.js
@@ -1,5 +1,17 @@
 var loremIpsum = require('lorem-ipsum')
+var randexp = require('randexp').randexp
 
 module.exports = function (schema) {
-  return loremIpsum()
+  var minLength = schema.minLength || 1
+  var maxLength = schema.maxLength || minLength + 10
+  var pattern = schema.pattern
+
+  if (pattern) {
+    return randexp(pattern)
+  } else {
+    var string = loremIpsum({
+      count: maxLength
+    })
+    return string.substr(0, Math.floor(Math.random() * maxLength + 1) + minLength  )
+  }
 }
--- tests/string.spec.js
+++ tests/string.spec.js
@@ -8,3 +8,41 @@ tape('string creates a string', function (t) {
   t.equal(typeof string(schema), 'string')
   t.end()
 })
+
+tape('string respects max character limit', function (t) {
+  var schema = {
+    type: 'string',
+    maxLength: 10
+  }
+  var allBelow = true
+  for (var i = 0; i < 100; i++) {
+    allBelow = string(schema).length <= schema.maxLength
+  }
+  t.equal(allBelow, true)
+  t.end()
+})
+
+tape('string respects min character limit', function (t) {
+  var schema = {
+    type: 'string',
+    minLength: 5
+  }
+  var allAbove = true
+  for (var i = 0; i < 100; i++) {
+    allAbove = string(schema).length > schema.minLength
+  }
+  t.equal(allAbove, true)
+  t.end()
+})
+
+tape('string will generate matches for regular expressions', function (t) {
+  var regex = '^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$'
+  var schema = {
+    type: 'string',
+    pattern: regex
+  }
+  var s = string(schema)
+  console.log(s)
+  t.equal(new RegExp(regex).test(s), true)
+  t.end()
+})
